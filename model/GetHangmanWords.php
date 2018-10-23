ba4d53e909fe1379578efa7c292bf4648e1cec1b
Sebastian Källstedt <sk223dn@student.lnu.se>
2018-10-23 19:03:40 +0200

    Started to work on the logic for checking if the guesses are correct in the word, and showing this

--- model/GetHangmanWords.php
+++ model/GetHangmanWords.php
@@ -10,43 +10,56 @@ require_once("environment.php");
 
 class GetHangmanWords {
     private static $link;
+    private $words;
 
 
     public function __construct(){
         self::$link = mysqli_connect($_ENV["DB_SERVER"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+        $this->connectToSql();
     }
  
-private function connectToSql() {
+    private function connectToSql() {
 
-// Check connection
-    if(self::$link === false){
-       die("ERROR: Could not connect. " . mysqli_connect_error());
-    } else {
-       return "Connection successfull";
-    }
-}
-
-public function sendMessage(){
-    return $this->connectToSql();
-}
-public function sql() {
- 
-        // Prepare a select statement
-        $sql = "SELECT * FROM words";
-        $words = [];
-        
-        $result = mysqli_query(self::$link, $sql);
-        // $row = mysqli_fetch_row($result, MYSQLI_NUM);
-        while($row = mysqli_fetch_assoc($result))
-        {
-            // var_dump($row["word"]);
-            array_push($words, $row["word"]);
+    // Check connection
+        if(self::$link === false){
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        } else {
+            return "Connection successfull";
         }
-        // var_dump($words);
-        return $words;
+    }
 
-        mysqli_free_result($result);
-    // Close connection
-    mysqli_close(self::$link);
-}
+    public function sendMessage(){
+        return $this->connectToSql();
+    }
+    public function sql() {
+ 
+            // Prepare a select statement
+            $sql = "SELECT * FROM words";
+            $this->words = [];
+        
+            $result = mysqli_query(self::$link, $sql);
+            // $row = mysqli_fetch_row($result, MYSQLI_NUM);
+            while($row = mysqli_fetch_assoc($result))
+            {
+                // var_dump($row["word"]);
+                array_push($this->words, $row["word"]);
+            }
+            // var_dump($words);
+            return $this->words;
+
+            mysqli_free_result($result);
+        // Close connection
+        mysqli_close(self::$link);
+    }
+
+    public function setCurrentGameWord()
+    {
+        $amountOfWords = count($this->words);
+        $randomNumber = rand(0, $amountOfWords -1);
+        var_dump($amountOfWords);
+        $randomWord = $this->words[$randomNumber];
+        $len = strlen($randomWord);
+        $theWordtoGuess = str_repeat('_ ', $len);
+        return $theWordtoGuess;
+    }
 }
--- view/HangmanView.php
+++ view/HangmanView.php
@@ -8,6 +8,8 @@ class HangmanView {
     private $hangmanWords;
     private $wordsArray;
     private $wordToGuess;
+    private $wrong;
+    private $guessedLetters;
 
     public function __construct(\model\HangmanStates $hangmanStates, \model\GetHangmanWords $hangmanWords)
     {
@@ -15,8 +17,10 @@ class HangmanView {
         $this->hangmanWords = $hangmanWords;
     }
 	public function show() {
-        $this->hangmanWords->sendMessage();
+        // $this->hangmanWords->sendMessage();
         $this->wordsArray = $this->hangmanWords->sql();
+        $this->wordToGuess = $this->hangmanWords->setCurrentGameWord();
+        $this->checkGuess();
         return 
         '<h1> Lets play Hangman!</h1>
          <pre>' 
@@ -25,6 +29,54 @@ class HangmanView {
          <br>
          <p><strong>Word to guess:'
          . $this->wordToGuess .
-         '</strong></p>';
-	}
+         '</strong></p>
+         <p>Letters you have guessed already:' 
+         . $_SESSION["guessedLetters"]
+         .$this->guessForm();
+    }
+    
+    public function guessForm()
+    {
+        return '
+        <form method="post" action="">
+           <input type="hidden" name="wrong" value='. $this->wrong .'/>
+           <fieldset>
+             <legend>Guess a word</legend>
+             <input type="text" name="letter" autofocus />
+             <input type="submit" value="Guess" /> 
+            </fieldset>
+        </form>';
+    }
+
+    public function checkGuess()
+    {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $word = $this->wordToGuess;
+
+            $currentGuess = $_POST["letter"];
+            $letter = strtoupper($currentGuess[0]);
+
+            if(!strstr($word, $letter))
+            {
+                if(isset($_SESSION["amountOfGuesses"])){
+                    $_SESSION["amountOfGuesses"] += 1;
+                } else {
+                    $_SESSION["amountOfGuesses"] = 1;
+                }
+            }
+
+            if(isset($_SESSION["guessedLetters"])) {
+                $_SESSION["guessedLetters"] = $_SESSION["guessedLetters"] . $letter;
+            } else {
+                $_SESSION["guessedLetters"] = $letter;
+            }
+            //$this->guessedLetters = $this->guessedLetters .= $letter;
+
+        }
+    }
+
+    public function checkGuessedLetter($guessedLetter)
+    {
+        $len = strlen($this->wordToGuess);
+    }
 }
